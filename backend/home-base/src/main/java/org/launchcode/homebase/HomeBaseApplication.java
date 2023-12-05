8b77b8e4ae9167608a03fd75c0e9634dbac63db6
Brittney Epperson <beppers87@hotmail.com>
2023-12-05 10:53:34 -0600

    Added Spring Boot back end

---
+++ backend/home-base/src/main/java/org/launchcode/homebase/HomeBaseApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.homebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeBaseApplication.class, args);
+	}
+
+}
---
+++ backend/home-base/src/test/java/org/launchcode/homebase/HomeBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.homebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
