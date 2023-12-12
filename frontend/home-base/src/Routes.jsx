28264232e7998e197523f17a31341cd8366351d6
TrickyLiam <liamcarlson2014@gmail.com>
2023-12-11 20:34:23 -0600

    Add landing page

--- frontend/home-base/src/Routes.jsx
+++ frontend/home-base/src/Routes.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Navigation from './components/Navigation';
+import Landing from './components/Landing';
 
 const Routing = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Landing />} />
         <Route path="/login" element={ <Login/> } />
-        <Route path="/navigation" element={ <Navigation/> } />
+        <Route path="/navigation" element={<Navigation />} />
       </Routes>
     </Router>
   );
