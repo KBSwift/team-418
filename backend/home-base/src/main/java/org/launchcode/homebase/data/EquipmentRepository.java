ba444f4710bee1474397557849f1b96249b99e14
Brittney Epperson <beppers87@hotmail.com>
2023-12-13 10:54:46 -0600

    Added ResourceNotFoundException, Equipment Controller, and added findByName method to Equipment repo

--- backend/home-base/src/main/java/org/launchcode/homebase/data/EquipmentRepository.java
+++ backend/home-base/src/main/java/org/launchcode/homebase/data/EquipmentRepository.java
@@ -4,6 +4,10 @@ import org.launchcode.homebase.models.Equipment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EquipmentRepository extends JpaRepository<Equipment, Integer> {
+
+    List<Equipment> findByName(String name);
 }
