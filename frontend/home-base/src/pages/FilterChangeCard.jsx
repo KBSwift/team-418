f322447b6ac00d3db222e11ee577d32be36a9669
Austin <kenjigw@gmail.com>
2023-12-16 13:41:57 -0600

    updated data load

--- frontend/home-base/src/pages/FilterChangeCard.jsx
+++ frontend/home-base/src/pages/FilterChangeCard.jsx
@@ -3,23 +3,55 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 
-function FilterChangeCard(){
+function FilterChangeCard({userId}){
 
+    // Load Data
 
-
+    const[data, setData] = useState(null);
+    const[loading, setLoading] = useState(true);
+    const[error, setError] = useState(null);
     
+    // the http request needs to fetch both equipment and filter data. Perhaps create custom query in back end to retrieve info based on user logged in. Example: `http://localhost:8080/api/user/${userId}/equipment-and-filters`
+
+    useEffect(() => {
+        axios.get(insert api endpoint)
+        .then(response => {
+            setData(response.data);
+            setLoading(false);
+        })
+        .catch(error =>{
+            setError(error);
+            setLoading(false);
+        });
+    }, [userId]); // userId in dependency array for useEffect hook
+        
+    if(loading) {
+        return <p>Loading...</p>
+    }
+
+    if(error) {
+        return <p>Encoutered error: {error.message}. Please try again.</p>
+    }
+
+    // Extract info from data response.
+    //TO-DO Test data pull and data structure
+
+    const equipmentName = data.equipment.name;
+    const filterLocation = data.location;
+    const filterSize = '${data.length} x ${data.width} x ${data.height}';
+    const dueDate = data.equipment.filterLifeDays;
+
     return(
         <div class="card" style={{width: '18rem'}}>
   <img></img>
  
-
  {/* TO-DO Fix the sample text to pull data */}
  {/* TO-DO Import Card into app.jsx & configure routing */}
   <div className="card-body">
-    <h5 className="card-title">Equipment Name</h5>
-    <h6 className="card-subtitle mb-2 text-muted">Equipment Location</h6>
-    <p className="card-text">Filter Size</p>
-    <p className="card-text">Due Date</p>
+    <h5 className="card-title">{equipmentName}</h5>
+    <h6 className="card-subtitle mb-2 text-muted">{filterLocation}</h6>
+    <p className="card-text">Filter Size: {filterSize}</p>
+    <p className="card-text">Due Date: {dueDate}</p>
     <button type="button" class="btn">Change Now</button>
   </div>
 </div>
