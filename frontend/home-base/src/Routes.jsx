23a8083f86a289f263a4c4a4865050c2f422eb5c
TrickyLiam <liamcarlson2014@gmail.com>
2023-12-29 10:23:43 -0600

    Add account setting page, consolidate views, change nav-bar

--- frontend/home-base/src/Routes.jsx
+++ frontend/home-base/src/Routes.jsx
@@ -4,15 +4,17 @@ import Login from './components/views/Signup';
 import NavigationBar from './components/NavigationBar';
 import Landing from './components/views/Landing';
 import Signup from './components/views/Signup';
-import FilterChangeCard from './pages/FilterChangeCard';
+import FilterChangeCard from './components/views/FilterChangeCard';
+import AccountSettings from './components/views/AccountSettings'
 
 const Routing = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/signup" element={ <Signup/> } />
+        <Route path="/signup" element={<Signup/>} />
         <Route path="/filter-change" element={<FilterChangeCard/>} />
+        <Route path="/account-settings" element={<AccountSettings/>} />
       </Routes>
     </Router>
   );
