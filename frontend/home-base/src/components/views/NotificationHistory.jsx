867eb7f1a1b7872414cad48e264d0a20171684af
Brittney Epperson <beppers87@hotmail.com>
2024-01-03 12:39:33 -0600

    Added notification history table styling

--- frontend/home-base/src/components/views/NotificationHistory.jsx
+++ frontend/home-base/src/components/views/NotificationHistory.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import { Table } from 'react-bootstrap';
+import '../styles/NotificationHistoryStyles.css';
 
 export const NotificationHistory = () => {
 
