30af9a5d37c229d4e6c6af1401149e626e0f1fbd
Austin <kenjigw@gmail.com>
2024-01-06 21:07:23 -0600

    refactored file names

--- backend/home-base/src/main/java/org/launchcode/homebase/controllers/EmailController.java
+++ backend/home-base/src/main/java/org/launchcode/homebase/controllers/EmailController.java
@@ -1,27 +1,26 @@
 package org.launchcode.homebase.controllers;
 
 import org.launchcode.homebase.models.EmailRequest;
-import org.launchcode.homebase.service.TestEmailService;
+import org.launchcode.homebase.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.sendgrid.helpers.mail.Mail;
 
 @RestController
 @RequestMapping("/api/email")
 public class EmailController {
     @Autowired
-    private TestEmailService testEmailService;
+    private EmailService emailService;
 
     @PostMapping("/send")
     public ResponseEntity<String> sendEmail(@RequestBody EmailRequest emailRequest)
 
     {
         try {
-            testEmailService.sendEmail(emailRequest.getEquipmentId(), emailRequest.getFilterId(), emailRequest.getTo(), emailRequest.getSubject(), emailRequest.getMessage());
+            emailService.sendEmail(emailRequest.getEquipmentId(), emailRequest.getFilterId(), emailRequest.getTo(), emailRequest.getSubject(), emailRequest.getMessage());
             return ResponseEntity.ok("Email sent successfully");
         } catch (Exception ex) {
             return ResponseEntity.badRequest().body("Failed to send email: " + ex.getMessage());
