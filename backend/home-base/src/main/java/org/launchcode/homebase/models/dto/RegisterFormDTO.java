21af82c81e60a66753070188095b3c12a735d2e4
Austin <kenjigw@gmail.com>
2024-01-07 11:21:44 -0600

    committing changes from pull

--- backend/home-base/src/main/java/org/launchcode/homebase/models/dto/RegisterFormDTO.java
+++ backend/home-base/src/main/java/org/launchcode/homebase/models/dto/RegisterFormDTO.java
@@ -1,25 +1,8 @@
-package org.launchcode.homebase.models.dto;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+package org.launchcode.homebase.models.DTO;
 
 public class RegisterFormDTO extends LoginFormDTO {
 
-    @NotNull
-    @NotBlank
-    @Size(min = 2, max = 30, message = "Invalid name. Must be between 2 and 30 characters.")
-    private String username;
-
     private String verifyPassword;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 
     public String getVerifyPassword() {
         return verifyPassword;
